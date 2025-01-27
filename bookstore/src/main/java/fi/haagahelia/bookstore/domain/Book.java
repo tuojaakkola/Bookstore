5ab9bd9de4bf9b3078beada89e4f69f176694773
TuomasJaakkola <bhm564@myy.haaga-helia.fi>
2025-01-27 10:11:18 +0200

    first commit

---
+++ bookstore/src/main/java/fi/haagahelia/bookstore/domain/Book.java
@@ -0,0 +1,14 @@
+package fi.haagahelia.bookstore.domain;
+
+public class Book {
+
+    private Long id;
+
+    public String title;
+    public String author;
+    public double price;
+    public String isbn;
+    public String publicationYear;
+
+    
+}
---
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -0,0 +1,17 @@
+package fi.haagahelia.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class BookController {
+
+    @GetMapping("/index")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+
+}
